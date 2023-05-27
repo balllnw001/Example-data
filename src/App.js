a3e60bc33dd233456dbc4099a28796c0c542a910
MSI\ballg <ballgoid50@gmail.com>
2023-05-27 13:01:26 +0700

    add Example 2 Products

--- src/App.js
+++ src/App.js
@@ -2,31 +2,6 @@ import './App.css';
 
 import Examdata from './Data/exam-data.json'
 
-//function App() {
-// const filteredProducts = Examdata.filter(Examdata => !Examdata.is_editable_price);
-// const totalWeight = filteredProducts.reduce((total, product) => total + product.weight, 0);
-// console.log(filteredProducts)
-// console.log(totalWeight)
-//console.log(Examdata)
-
-{/*return (
-    <div className="App">
-       {filteredProducts.map(record => {
-        let totalWeight = 0;
-
-        record.products.forEach(Weightproduct => {
-          totalWeight += Weightproduct.weight;
-        });
-
-        return (
-          <div className='box' key={record.id}>
-            Name {record.name} TotalWeight {totalWeight}
-          </div>
-        );
-      })} 
-    </div>
-  );*/}
-//}
 const App = () => {
   const filteredProducts = Examdata.filter(Examdata => !Examdata.is_editable_price);
   const total = filteredProducts.reduce((accumulator, product) => {
@@ -40,15 +15,30 @@ const App = () => {
 
   const jsonData = JSON.stringify(total);
 
+  console.log(filteredProducts);
   console.log(total);
   console.log(jsonData);
 
   return (
     <div>
-      <h1>Example Products</h1>
+      <h1>Example 1 Products</h1>
       <div key={jsonData}>
         <pre>{jsonData}</pre>
       </div>
+      <h1>Example 2 Products</h1>
+      {filteredProducts.map(record => {
+        let totalWeight = 0;
+
+        record.products.forEach(Weightproduct => {
+          totalWeight += Weightproduct.weight;
+        });
+
+        return (
+          <div className='box' key={record.id}>
+            Name {record.name} TotalWeight {totalWeight}
+          </div>
+        );
+      })}
     </div >
   );
 };
